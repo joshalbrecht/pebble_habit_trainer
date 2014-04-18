7f0b81cc77f05ac675c6008a55f68a0064d3c398
Josh Albrecht <thejash@gmail.com>
2014-04-18 10:48:09 -0700

    fixed a lot of little annoying things. now actually usable for me at least.

--- src/habit_trainer.c
+++ src/habit_trainer.c
@@ -7,25 +7,31 @@ static AppTimer *timer;
 static bool timer_running;
 static int seconds_left;
 static int interval;
+static const int MIN_SECONDS = 10;
+static const int STARTING_INTERVAL = 10;
 
 static void decrease_timer() {
   interval = interval / 2;
-  if (interval < 4) {
-    interval = 4;
+  if (interval < MIN_SECONDS) {
+    interval = MIN_SECONDS;
   }
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Decreased timer to %d", interval);
   seconds_left = interval;
 }
 
 static void increase_timer() {
-  interval = interval * 2;
+  interval = (int)((float)(interval) * 1.5);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Increased timer to %d", interval);
   seconds_left = interval;
 }
 
 static void update_countdown() {
-  static char text_buffer[] = "100000000 seconds left";
-  snprintf(text_buffer, sizeof(text_buffer), "%d seconds left", seconds_left);
+  static char text_buffer[] = "Press up or down to extend the timer.\nPress select to decrease the interval.";
+  if (seconds_left > 0) {
+    snprintf(text_buffer, sizeof(text_buffer), "%d seconds left", seconds_left);
+  } else {
+    snprintf(text_buffer, sizeof(text_buffer), "Press up or down to extend the timer.\nPress select to decrease the interval.");
+  }
   text_layer_set_text(text_layer, text_buffer);
 }
 
@@ -33,8 +39,6 @@ static void tick_down(void* data) {
   seconds_left = seconds_left - 1;
   update_countdown();
   if (seconds_left == 0) {
-    vibes_short_pulse();
-    vibes_long_pulse();
     vibes_double_pulse();
   }
   if (seconds_left < -10) {
@@ -50,7 +54,7 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
     }
   } else {
     timer_running = true;
-    interval = 5;
+    interval = STARTING_INTERVAL;
     seconds_left = interval;
     update_countdown();
     timer = app_timer_register(1000, tick_down, NULL);
@@ -64,6 +68,9 @@ static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  if (timer_running && seconds_left <= 0) {
+    increase_timer();
+  }
 }
 
 static void click_config_provider(void *context) {
@@ -76,9 +83,10 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, bounds.size.h } });
   text_layer_set_text(text_layer, "Click select to start");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeTrailingEllipsis);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
